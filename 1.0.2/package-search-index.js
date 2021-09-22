31f418e7b86d2e05f6fdd406f6b87b19d7fd2d42
Goldmensch <47005420+Goldmensch@users.noreply.github.com>
2021-09-22 14:04:03 +0000

    Deploying to docs from @ Goldmensch/JALL@ec0cc1fd68b4befa40d808a02a28f36b3079f74f 🚀

---
+++ 1.0.2/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.goldmensch.jall"},{"l":"io.github.goldmensch.jall.exceptions"},{"l":"io.github.goldmensch.jall.placeholder"},{"l":"io.github.goldmensch.jall.registry"},{"l":"io.github.goldmensch.jall.transformer"}];updateSearchResults();
